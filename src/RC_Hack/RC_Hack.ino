4706d03a26decd84db9af29469b5298dde583267
Toomai007 <frederiks007@gmail.com>
2013-10-23 20:07:00 +0200

    1.0.0 beta test

--- src/RC_Hack/RC_Hack.ino
+++ src/RC_Hack/RC_Hack.ino
@@ -17,7 +17,10 @@ int left = 11;
 int right = 10;
 
 //Sensor
-int converted_sensor_distance = null;
+int converted_sensor_distance = 0;
+int pingTimer = 30;
+int min_sensor_distance = 30;
+
 
 void setup() {
   //RC controls
@@ -25,11 +28,32 @@ void setup() {
   pinMode(backwards, OUTPUT);
   pinMode(left, OUTPUT);
   pinMode(right, OUTPUT);
-  
-  timer_since_start = millis();    //Starts timer since program startup.
 }
 
 void loop() {
-  unsigned long sensor_distance = sonar.ping();    //Updates distance of ultraonic sensor
-  converted_sensor_distance = sensor_distance / US_ROUNDTRIP_CM;    //Converts to CM
+  if (millis() >= pingTimer) {   //pingSpeed milliseconds since last ping, do another ping.
+    unsigned long sensor_distance = sonar.ping();    //Updates distance of ultraonic sensor
+    converted_sensor_distance = sensor_distance / US_ROUNDTRIP_CM;    //Converts to CM
+  }
+  
+  if (converted_sensor_distance < min_sensor_distance){
+    digitalWrite(forward, LOW);
+    
+    //Turn
+    digitalWrite(left, HIGH);
+    digitalWrite(backwards, HIGH);
+    delay(300);
+    digitalWrite(left, LOW);
+    digitalWrite(backwards, LOW);
+    delay(100);
+    
+    digitalWrite(right, HIGH);
+    digitalWrite(forward, HIGH);
+    delay(300);
+    digitalWrite(right, LOW);
+    digitalWrite(forward, LOW);
+    delay(100);
+  } else {
+    digitalWrite(forward, HIGH);
+  }
 }
